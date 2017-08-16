e8729cc5329f769d0904399d9708bd54207bc051
FLASH102 <FLASH102@users.noreply.github.com>
2017-08-15 21:31:58 -0700

    Moved notes and hmeowrk

---
+++ CalculatorSample/src/Application.java
@@ -0,0 +1,12 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator C = new Calculator();
+		int product = C.multiply(359, 4777);
+
+		System.out.println(product);
+	}
+
+}
---
+++ CalculatorSample/src/Calculator.java
@@ -0,0 +1,10 @@
+
+public class Calculator{
+	int multiply(int x, int y) {
+	int	a;
+	a = x*y;
+	return a;
+	}
+	
+
+}
