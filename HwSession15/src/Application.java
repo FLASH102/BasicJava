732fb866cae4bb10a5dd3d05ffd0efbc54c00f0c
FLASH102 <FLASH102@users.noreply.github.com>
2017-11-03 20:25:51 -0700

    Session15HW

---
+++ HwSession15/src/Application.java
@@ -0,0 +1,16 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Student s1 = new Student("Bhavika", 7);
+s1.writeEssay("Environmental Issues");
+
+Food f2 = new Food("Bob", 30);
+f2.makeDinner("Tacos", 5);
+
+	}
+	
+
+}
